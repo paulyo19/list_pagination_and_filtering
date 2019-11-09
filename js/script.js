1f26ae6bf24b0feebad6bdbd42e0af537840e1e0
paulyo19 <catkillercleary@gmail.com>
2019-11-09 21:14:22 +0000

    deleting more comments

--- js/script.js
+++ js/script.js
@@ -1,32 +1,9 @@
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+
 const studentList = document.getElementsByClassName('student-item')
 const numPages = 10
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 const showPage = (list, page) => {
    let startIndex = (page * 10) - 10;
